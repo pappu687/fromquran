3b0e5d04d50ccad1f180dcbafe4a1b27f367a980
Md. Mahbubur Rahman <pappu687@users.noreply.github.com>
2026-03-16 06:22:17 +0600

    Changed favicon

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -35,7 +35,7 @@
 
         <link rel="icon" href="/favicon.svg" type="image/svg+xml">
         <link rel="icon" href="/favicon.ico" sizes="any">
-        <link rel="apple-touch-icon" href="/favicon.svg">
+        <link rel="apple-touch-icon" href="/apple-touch-icon.png">
 
         <link rel="preconnect" href="https://fonts.bunny.net">
         <link href="https://fonts.bunny.net/css?family=instrument-sans:400,500,600" rel="stylesheet" />
